27188c770e00d2e176e88d8a6dedfc94b14a89e2
Anton Å <anton.asbrink@hotmail.com>
2020-06-23 13:07:00 +0200

    Added a config class with txt file
    includes changing window size

---
+++ src/Config.cpp
@@ -0,0 +1,54 @@
+#include "Config.h"
+#include <fstream>
+#include <iostream>
+
+enum {
+	INVALID,
+	WINDOW_HEIGHT,
+	WINDOW_WIDTH
+};
+
+Config::Config()
+{
+	std::ifstream file("config.txt");
+
+	if (file.is_open()) {
+		while (!file.eof()) {
+			std::string variable;
+			std::string value;
+			std::getline(file, variable, '=');
+			variable.erase(std::remove_if(variable.begin(), variable.end(), std::isspace));
+			std::getline(file, value);
+
+			switch (getVariableID(variable)) {
+			case WINDOW_HEIGHT:
+				m_windowHeight = std::stoi(value);
+				break;
+			case WINDOW_WIDTH:
+				m_windowWidth = std::stoi(value);
+				break;
+			default:
+				std::cout << "Unknown variable \"" << variable << "\" with value \"" << value << "\"";
+				break;
+			}
+		}
+	}
+}
+
+int Config::getVariableID(std::string str)
+{
+	if (std::strcmp(str.c_str(), "windowSizeWidth") == 0) return WINDOW_WIDTH;
+	if (std::strcmp(str.c_str(), "windowSizeHeight") == 0) return WINDOW_HEIGHT;
+
+	return INVALID;
+}
+
+int Config::getWindowSizeWidth()
+{
+	return m_windowWidth;
+}
+
+int Config::getWindowSizeHeight()
+{
+	return m_windowHeight;
+}
